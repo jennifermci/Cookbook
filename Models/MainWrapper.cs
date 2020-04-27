528a42478b98b887eb0a9c91b610c826fbf3442d
Jennifer McIlvaine <jennymci22@gmail.com>
2020-04-27 16:09:21 -0700

    do over

---
+++ Models/MainWrapper.cs
@@ -0,0 +1,12 @@
+using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Collections.Generic;
+
+namespace Cookbook.Models
+{
+    public class MainWrapper
+    {
+        public RegisterUser RegisterUser {get;set;}
+    }
+}
---
+++ Models/Tip.cs
@@ -0,0 +1,23 @@
+using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Collections.Generic;
+
+namespace Cookbook.Models
+{
+    public class Tip
+    {
+        [Key]
+        public int TipId {get; set;}
+        [Required]
+        public string Text {get;set;}
+
+        public int UserId {get;set;}
+        public RegisterUser User {get;set;}
+        public List<Comment> Comments {get;set;}
+        public List<Like> Likes {get;set;}
+        public DateTime CreatedAt { get; set; } = DateTime.Now;
+        public DateTime UpdatedAt { get; set; } =  DateTime.Now;
+
+    }
+}
